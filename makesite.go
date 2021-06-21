0ea850375d270ea54d86a6dc00686a2309f9fc7b
Francis Tsang <frtsang40@gmail.com>
2021-06-21 12:20:33 -0700

    finished v1.1

--- makesite.go
+++ makesite.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 	"text/template"
@@ -17,21 +17,30 @@ type Page struct {
 func main() {
 	// defining flag
 	var filename string
+	var dir string
 
 	//define the flag, using the pointer to filename variable
 	flag.StringVar(&filename, "file", "", "Text file name")
+	flag.StringVar(&dir, "dir", "", "Directory Name")
 	flag.Parse()
-	if filename == "" {
-		fmt.Println("Why")
-		return
+
+	// Uses other functions that were created depending on file on directory
+	if dir != "" {
+		dirConverter(dir)
+	} else if filename != "" {
+		fileConverter(filename)
 	}
+}
 
+func fileConverter(filename string) {
 	fileContents, err := ioutil.ReadFile(filename)
-	if err != nil {
 
+	// Return error vakue that went unhandled
+	if err != nil {
 		panic(err)
 	}
 
+	// Takes txt file seperates from .txt and slaps a .html on it
 	fileCreator, err := os.Create(strings.SplitN(filename, ".", 2)[0] + ".html")
 	if err != nil {
 		panic(err)
@@ -44,4 +53,17 @@ func main() {
 	}
 
 	fileCreator.Close()
+}
+
+func dirConverter(directory string) {
+	files, err := ioutil.ReadDir(directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if file.Name()[len(file.Name())-3:] == "txt" {
+			fileConverter(file.Name())
+		}
+	}
 }
