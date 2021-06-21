0cb5749e76a21da57d39fdf0df777c81059c9373
Francis Tsang <frtsang40@gmail.com>
2021-06-21 12:30:36 -0700

    initialized go module

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module makesite
 
 go 1.13
+
+require (
+	github.com/microcosm-cc/bluemonday v1.0.14
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+)
--- makesite.go
+++ makesite.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 	"text/template"
+
+	"github.com/microcosm-cc/bluemonday"
 )
 
 type Page struct {
@@ -30,6 +33,21 @@ func main() {
 	} else if filename != "" {
 		fileConverter(filename)
 	}
+
+	// Test code for bluemonday
+
+	// Do this once for each unique policy, and use the policy for the life of the program
+	// Policy creation/editing is not safe to use in multiple goroutines
+	p := bluemonday.UGCPolicy()
+
+	// The policy can then be used to sanitize lots of input and it is safe to use the policy in multiple goroutines
+	html := p.Sanitize(
+		`<a onblur="alert(secret)" href="http://www.google.com">Google</a>`,
+	)
+
+	// Output:
+	// <a href="http://www.google.com" rel="nofollow">Google</a>
+	fmt.Println(html)
 }
 
 func fileConverter(filename string) {
