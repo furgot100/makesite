1fb0339cf5195c0e67d53808b98cd05368298d4a
Francis Tsang <frtsang40@gmail.com>
2021-06-21 11:35:33 -0700

    able to render file in stdout

--- makesite.go
+++ makesite.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"text/template"
+)
+
+type entry struct {
+	Name string
+	Done bool
+}
+
+type ToDo struct {
+	User string
+	List []entry
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	fileContents, err := ioutil.ReadFile("first-post.txt")
+	if err != nil {
+
+		panic(err)
+	}
+	fmt.Print(string(fileContents))
+
+	t := template.Must(template.New("template.tmpl").ParseFiles("new.html"))
+	err = t.Execute(os.Stdout, string(fileContents))
+	if err != nil {
+		panic(err)
+	}
 }
