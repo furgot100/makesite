7e81d69dfa020114c28798b9ef1d89ac3b354c58
Francis Tsang <frtsang40@gmail.com>
2022-02-04 16:39:39 -0800

    finished generating and writing onto new html file

--- makesite.go
+++ makesite.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"os"
+	"text/template"
+)
+
+type entry struct {
+	Name string
+	Done bool
+}
+
+type ToDo struct {
+	User string
+	List []entry
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	fileContents, err := ioutil.ReadFile("first-post.txt")
+	if err != nil {
+		panic(err)
+	}
+	f, err := os.Create("first-post.html")
+	if err != nil {
+		panic(err)
+	}
+
+	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+	err = t.Execute(f, string(fileContents))
+	if err != nil {
+		panic(err)
+	}
+	f.Close()
 }
